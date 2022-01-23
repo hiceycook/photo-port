e1a6fe35eecb90cd02bb6e5024634dfbd0655acf
hicey <hiceycook@gmail.com>
2022-01-23 15:38:20 -0500

    form handles

--- src/components/Conact/index.js
+++ src/components/Conact/index.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function ContactForm() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formState;
+
+    function handleChange(e) {
+        setFormState({ ...formState, [e.target.name]: e.target.value })
+    };
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
 
     return (
         <section>
             <h1>Contact me</h1>
-            <form id="contact-form">
+            <form onSubmit={handleSubmit} id="contact-form">
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    <input onChange={handleChange} type="text" name="name" defaultValue={name} />
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" />
+                    <input onChange={handleChange} type="email" name="email" defaultValue={email} />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" />
+                    <textarea onChange={handleChange} name="message" defaultValue={message} rows="5" />
                 </div>
                 <button type="submit">Submit</button>
             </form>
